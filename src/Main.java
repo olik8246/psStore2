b117254d453d2c5681388e265f4a0fa8a4469108
olik8246 <126155704+olik8246@users.noreply.github.com>
2023-06-01 11:11:44 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Ps valakas = new Ps("R5 5600X","1Tb","RTX3060 12gb ","500Gb","asus","16GB","600w",36000);
+        Ps valakas = new Ps("R5 5600X","1Tb","RTX3060 12gb ","500Gb","asus","16GB","600w",36000);//«Створення власних класів. Застосування конструкторів»
         Ps vortex = new Ps("i5-10400F","1Tb","GTX 1050 6Gb ","240Gb","asus","16GB","600w",21000);
         Ps huananzhi = new Ps("Xeon E5 2658","500Gb","RX 480 8Gb ","256Gb","asus","16GB","600w",11000);
         Buyer buyer = new Buyer();
@@ -21,17 +21,17 @@ public class Main {
             case 1:
                 System.out.println("Ви вибрали Valakas");
                 valakas.discount();
-                confirm.setName("Valakas");
+                confirm.setName("Valakas");// перевизначений метод
                 break;
             case 2:
                 System.out.println("Ви вибрали Vortex");
                 vortex.discount();
-                confirm.setName("Vortex");
+                confirm.setName("Vortex");// перевизначений метод
                 break;
             case 3:
                 System.out.println("Ви вибрали Huananzhi");
                 huananzhi.discount();;
-                confirm.setName("Huananzhi");
+                confirm.setName("Huananzhi");// перевизначений метод
                 break;
             default:
                 System.out.println("папа).");
